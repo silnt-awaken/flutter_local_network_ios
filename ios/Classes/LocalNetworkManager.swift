7b00eb535726a888d25ddc3ac024f9f366bdf0a5
Michael G <silnt@Michaels-Mac-mini.local>
2024-11-15 10:49:35 -0500

    asdf

--- ios/Classes/LocalNetworkManager.swift
+++ ios/Classes/LocalNetworkManager.swift
@@ -20,31 +20,22 @@ class LocalNetworkManager: NSObject {
 @available(iOS 14.0, *)
 private extension LocalNetworkManager {
     func iOS14_requestAuthorization() {
-        // Replace with your router's IP address
-        let hostIP = "10.0.0.1"
-        let urlString = "http://\(hostIP)/"
+        let browser = NetServiceBrowser()
+        browser.delegate = self
+        browser.searchForServices(ofType: "_http._tcp.", inDomain: "local.")
+    }
+}
 
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL")
-            authResult?(false)
-            return
-        }
+extension LocalNetworkManager: NetServiceBrowserDelegate {
+    func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
+        print("Found service: \(service)")
+        authResult?(true)
+        browser.stop()
+    }
 
-        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            if let error = error {
-                print("HTTP request failed: \(error)")
-                DispatchQueue.main.async {
-                    self?.authResult?(false)
-                }
-                return
-            }
-
-            print("HTTP request succeeded")
-            DispatchQueue.main.async {
-                self?.authResult?(true)
-            }
-        }
-
-        task.resume()
+    func netServiceBrowser(_ browser: NetServiceBrowser, didNotSearch errorDict: [String : NSNumber]) {
+        print("Failed to search: \(errorDict)")
+        authResult?(false)
+        browser.stop()
     }
 }
